7af6f7776e4baa333c3695f319814a1b78a32def
Byeolah-Kwon <byeolahk@gmail.com>
2026-09-22 17:55:56 -0400

    configure github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/dock-scheduling-system",
+  assetPrefix: "/dock-scheduling-system/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
